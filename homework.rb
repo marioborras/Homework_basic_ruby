7632b46ddd3cd1cb90ea200a4738212e107a67bb
Mario Borras <mario.borras@icloud.com>
2018-08-22 20:59:51 -0400

    fixed

--- homework.rb
+++ homework.rb
@@ -7,15 +7,14 @@ america("anything")
 
 #Create a function to find the maximum value in an array of numbers. For instance: [100,10,-1000] should return 100. Do not use Ruby's built-in .max function.
 def maxValue(arr)
-
-puts arr.sort.last
+    puts arr.sort.last
 end
 maxValue([100,10,-1000])
 #Create a function that takes two arguments - both of them arrays. Inside of the function, combine the arrays using the items from the first array as keys and the second array as values. For example, when these two arrays are supplied as arguments: 
 
 def combiner(make,model)
     hash = {}
-   make.each_with_index do |key,index| # for each item in the array
+   make.each_with_index do |key,index| # for each item in the array.
     hash[key] = model[index] #run this block of code - the current item is aliased to the variable name                                 "index" /Slide 20, Ruby Basics
    end
     puts hash
